98f35c0d585a8340a8772e2106fb4385f97931d7
Adrian Arumugam <apa@moogzy.net>
2014-07-23 23:17:24 +1000

    Committing final C3-E1 - binary converter

--- class3/binary_converter.py
+++ class3/binary_converter.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python
 
+""" PyNet - Python for Network Engineers
+
+    Class 3 - Exercise 1
+
+    Author: Adrian Arumugam
+    Date 23/07/2014
+
+"""
+
 import sys
 
+# Verify arguments passed to command line
 if len(sys.argv) == 2:
+    
+    # Pop off the IP address and assign to variable
     ip_addr = sys.argv.pop()
+
+    # Split the IP address on the decimal for further processing
     split_addr = ip_addr.split(".")
+
+    # Iterate over the split ip address list
+    #
+    # Convert each list entry into binary equivalent.
+    # Strip leading "0b" from the binary value.
+    # Enter a while loop to pad each binary value with leading "0s"
+    # until binary value is eight digits in length.
     for index, octet in enumerate(split_addr):
        split_addr[index] = bin(int(octet))
-    print "%20s %20s %20s %20s" % ("first_octet", "second_octet", "third_octet", "fourth_octet")
-    print "".join(split_addr]
+       split_addr[index] = split_addr[index].lstrip("0b")
+       while len(split_addr[index]) <= 7:
+          split_addr[index] = "".join(("0", split_addr[index]))
+
+    # Print out results using format strings.
+    print "%-20s %-s" % ("IP address", "Binary")
+    print "%-20s %-s.%-s.%s.%s" % (ip_addr, split_addr[0], split_addr[1], split_addr[2], split_addr[3])
